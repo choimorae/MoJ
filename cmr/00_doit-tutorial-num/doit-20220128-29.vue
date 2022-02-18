4e0f49ddef3473cea0e5f0fc117a3c9f1f87c1a5
cmr <cmr@minisoft.co.kr>
2022-02-18 16:25:38 +0900

    여태까지 튜토리얼 한 파일에 정리 + 뷰 프로젝트 임시 제작

---
+++ cmr/00_doit-tutorial-num/doit-20220128-29.vue
@@ -0,0 +1,13 @@
+<template>
+<div><span><button>{{message}}</button></span></div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style>
+    span{
+        font-size:0.75rem;
+    }
+</style> 
